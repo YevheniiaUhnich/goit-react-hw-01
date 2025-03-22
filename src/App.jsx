19fea84ed1e8c91d0be4821c058a714307678c2d
YevheniiaUhnich <140949326+YevheniiaUhnich@users.noreply.github.com>
2025-03-22 22:05:27 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,18 @@ import Profile from "./components/Profile/Profile";
 
 const App = () => {
   return (
-    <Profile
-      userName={userData.username}
-      tag={userData.tag}
-      location={userData.location}
-      avatar={userData.avatar}
-      stats={userData.stats}
-    />
+    <>
+      <h2 className="titleTask">Task-1</h2>
+      <Profile
+        userName={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
+
+      <h2 className="titleTask">Task-1</h2>
+    </>
   );
 };
 export default App;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,21 +1,29 @@
 import s from "./Profile.module.css";
+import clsx from "clsx";
+import { FcLike } from "react-icons/fc";
+import { SlUserFollow } from "react-icons/sl";
+import { GrOverview } from "react-icons/gr";
 
 const Profile = ({ userName, tag, location, avatar, stats }) => {
   return (
-    <div className="profile">
-      <img src={avatar} alt="User avatar" />
-      <p>{userName}</p>
-      <p>{tag}</p>
-      <p>{location}</p>
-      <ul>
-        <li>
-          <span>Followers</span> {stats.followers}
+    <div className={s.profile}>
+      <img className={s.avatar} src={avatar} alt="User avatar" />
+      <p className={s.nameProfile}>{userName}</p>
+      <p className={s.nickName}>@{tag}</p>
+      <p className={s.locationProfile}>{location}</p>
+      <ul className={s.ulProfile}>
+        <li className={s.liProfile}>
+          <SlUserFollow className={s.iconFol} />
+          <span className={s.spanFollowers}>Followers</span> {stats.followers}
         </li>
-        <li>
-          <span>Views</span> {stats.views}
+        <li className={s.liProfile}>
+          <GrOverview className={s.views} />
+          <span className={s.spanFollowers}>Views</span> {stats.views}
         </li>
-        <li>
-          <span>Likes</span> {stats.likes}
+
+        <li className={clsx(s.liProfile, s.red)}>
+          <FcLike className={s.icon} />
+          <span className={s.spanFollowers}>Likes</span> {stats.likes}
         </li>
       </ul>
     </div>
