884b00910febc63851373a5b75217b012250837e
YevheniiaUhnich <140949326+YevheniiaUhnich@users.noreply.github.com>
2025-03-23 07:31:21 +0200

    task-2

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,14 +1,20 @@
 import s from "./FriendList.module.css";
+import clsx from "clsx";
 
 const FriendList = ({ friends }) => {
   return (
     <ul className={s.friendList}>
       {friends.map(({ id, avatar, name, isOnline }) => (
         <li key={id} className={s.friendItem}>
-          <span
-            className={`${s.status} ${isOnline ? s.online : s.offline}`}></span>
-          <img src={avatar} alt="name" width="88" />
+          <img src={avatar} alt="name" width="120" />
           <p className={s.titleName}>{name}</p>
+          {isOnline ? (
+            <span className={clsx(s.status, s.green)}>Online</span>
+          ) : (
+            <span className={clsx(s.status, s.red)}>Offline</span>
+          )}
+          {isOnline && <span></span>}
+          {}
         </li>
       ))}
     </ul>
