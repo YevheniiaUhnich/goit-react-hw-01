8012c57c164402e2613c746f0e35327f598b933b
YevheniiaUhnich <140949326+YevheniiaUhnich@users.noreply.github.com>
2025-03-23 13:31:05 +0200

    FriendListItem

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import userData from "./userData.json";
 import friends from "./friends.json";
 import transactions from "./transactions.json";
 import Profile from "./components/Profile/Profile";
-import FriendList from "./components/FriendList/FriendList";
+import FriendListItem from "./components/FriendList/FriendList";
 import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 
 const App = () => {
